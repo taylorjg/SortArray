5abc3d926fd6e95663d6c46aca416052ce9c35e1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-07-01 11:19:24 +0100

    Got a basic sort working

--- src/kata.js
+++ src/kata.js
@@ -1,4 +1,21 @@
 function sortArray(xs) {
+
+    function swap(i1, i2) {
+        const t = xs[i2];
+        xs[i2] = xs[i1];
+        xs[i1] = t;
+        return true;
+    }
+
+    for (;;) {
+        let swapped = false;
+        for (let i = 0; i + 1 < xs.length; i++) {
+            if (xs[i] > xs[i + 1]) {
+                swapped = swap(i, i + 1);
+            }
+        }
+        if (!swapped) break;
+    }
     return xs;
 }
 
--- test/kata.spec.js
+++ test/kata.spec.js
@@ -4,16 +4,17 @@ import it_multiple from './it_multiple';
 describe('kata unit tests', () => {
 
     const testCases = [
-        [[0]], 
-        [[0, 0]], 
-        [[1, 2, 3]], 
+        [[0]],
+        [[0, 0]],
+        [[1, 2, 3]],
+        [[3, 2, 1]]
     ];
 
     it_multiple(
         'sortArray',
         xs => {
-            const expected = xs.sort();
-            expect(sortArray(xs)).toBe(expected);
+            const expected = xs.slice().sort();
+            expect(sortArray(xs)).toEqual(expected);
         },
         testCases);
 });
